be655d4f484f0483767f89dc4ebf3ce84f21c083
Eduardo Batista <eduardo_batistaalves@hotmail.com>
2024-06-30 13:08:55 -0300

    chore: add .gitignore

--- deploy-config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Configurações de deploy
-$config = [
-    'repo_url' => 'outra',
-    'webhook_secret' => 'outra',
-    'target_dir' => '/home/eduardo/projects/zip-auto-deploy',
-    'temp_dir' => '/home/eduardo/projects/zip-auto-deploy/temp_clone',
-    'log_file' => '/home/eduardo/projects/zip-auto-deploy/logs-zip/deploy.log',
-];
