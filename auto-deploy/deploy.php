5a13f7bc6467926eaf76b69ee8d7d4db45037b15
Eduardo Batista <eduardo_batistaalves@hotmail.com>
2024-07-16 12:36:06 -0300

    fix: not create lockfile with github ping test

--- auto-deploy/deploy.php
+++ auto-deploy/deploy.php
@@ -8,22 +8,22 @@ $lockFile = dirname(__DIR__) .'/deploy.lock';
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-// Incluir o arquivo de configuração
+
 
 include dirname(__DIR__) . '/deploy-config.php';
 
-// Verificar se $config foi definido corretamente
+
 if (!isset($config)) {
     die("Erro: arquivo de configuração não carregado corretamente.");
 }
 
 
-// Utilizar as configurações do arquivo
+
 $deployScriptShell = dirname(__DIR__) . '/auto-deploy/deploy.sh';
 $logFile = $config['log_file'];
 $githubSecret = $config['webhook_secret'];
 
-// Função para mensagens de log
+
 function logMessage($message) {
     global $logFile;
     $timestamp = date("Y-m-d H:i:s");
@@ -45,7 +45,7 @@ $githubEvent = $_SERVER['HTTP_X_GITHUB_EVENT'];
 $githubDelivery = $_SERVER['HTTP_X_GITHUB_DELIVERY'];
 $githubSignature = $_SERVER['HTTP_X_HUB_SIGNATURE_256'];
 
-// Verificação da assinatura HMAC
+
 $signature = 'sha256=' . hash_hmac('sha256', $payload, $githubSecret);
 
 if (!hash_equals($githubSignature, $signature)) {
@@ -61,20 +61,24 @@ if (file_exists($lockFile)) {
 }
 
 // Criar o arquivo de lock
-$lockHandle = fopen($lockFile, 'w');
-if ($lockHandle === false || !flock($lockHandle, LOCK_EX | LOCK_NB)) {
+if ($githubEvent == 'push') {   $lockHandle = fopen($lockFile, 'w');
+    if ($lockHandle === false || !flock($lockHandle, LOCK_EX | LOCK_NB)) {
     logMessage("Não foi possível criar ou bloquear o arquivo de lock. Verifique as permissões.");
     die("Não foi possível criar ou bloquear o arquivo de lock. Verifique as permissões.");
+    }
+} else {
+    logMessage("Não foi um evento de push.");
+    die("Não foi um evento de push.");
 }
 
 logMessage('Iniciando o deploy');
 
-// Verifica se o evento é um push
+
 if ($githubEvent == 'push') {
-    // Executa o script shell de deploy
+  
     $output = shell_exec("bash " . escapeshellarg($deployScriptShell) . " 2>&1");
     
-    // Log da execução do deploy
+    
     file_put_contents($logFile, "[" . date('Y-m-d H:i:s') . "] Resultado do deploy:\n$output\n\n", FILE_APPEND);
 
     echo "Deploy executado com sucesso!";
